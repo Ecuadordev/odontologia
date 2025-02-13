e0066b79aed4b1b5468906a5593bae75714b0933
Dario Chuquilla <dario.chuquilla@gmail.com>
2025-02-13 11:36:07 -0500

    Add new columns to paciente_exploracion and paciente_consulta tables

--- sql_migrations.sql
+++ sql_migrations.sql
@@ -1,2 +1,8 @@
 ALTER TABLE paciente_exploracion
 ADD COLUMN estomgtc_exp VARCHAR(255);
+
+ALTER TABLE paciente_consulta
+ADD COLUMN health_piece VARCHAR(255);
+
+ALTER TABLE paciente_consulta
+ADD COLUMN pcg VARCHAR(255);
