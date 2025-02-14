0967a1e3cc0c822075a68bfce4024b728bbc5f0d
Dario Chuquilla <dario.chuquilla@gmail.com>
2025-02-14 01:59:08 -0500

    Add paciente_planes_diagnostico table with foreign key to paciente

--- sql_migrations.sql
+++ sql_migrations.sql
@@ -18,3 +18,18 @@ ADD COLUMN flourosis VARCHAR(50);
 
 ALTER TABLE paciente_consulta
 ADD COLUMN cpo VARCHAR(255);
+
+CREATE TABLE
+  `paciente_planes_diagnostico` (
+    `pacplandiag_id` int (8) NOT NULL AUTO_INCREMENT,
+    `codi_pac` int (8) DEFAULT NULL,
+    `enfermedades` varchar(255) CHARACTER
+    SET
+      utf8 DEFAULT NULL,
+      `observaciones` varchar(255) CHARACTER
+    SET
+      utf8 DEFAULT NULL,
+      PRIMARY KEY (`pacplandiag_id`),
+      KEY `codi_pac` (`codi_pac`),
+      CONSTRAINT `paciente_plan_diagnostico_ibfk_1` FOREIGN KEY (`codi_pac`) REFERENCES `paciente` (`codi_pac`) ON DELETE CASCADE ON UPDATE CASCADE
+  ) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8 COLLATE = utf8_spanish_ci;
