b682a1cba411393507c5e4fc5cee9d10e81e2af1
Dario Chuquilla <dario.chuquilla@gmail.com>
2025-02-16 16:48:52 -0500

    Add predef column to paciente_diagnostico table and update edit functionality

--- sql_migrations.sql
+++ sql_migrations.sql
@@ -33,3 +33,6 @@ CREATE TABLE
       KEY `codi_pac` (`codi_pac`),
       CONSTRAINT `paciente_plan_diagnostico_ibfk_1` FOREIGN KEY (`codi_pac`) REFERENCES `paciente` (`codi_pac`) ON DELETE CASCADE ON UPDATE CASCADE
   ) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8 COLLATE = utf8_spanish_ci;
+
+ALTER TABLE paciente_diagnostico
+ADD COLUMN predef VARCHAR(255);
