ca42d793c8c287134143ce86f10a317ebefaf874
Dario Chuquilla <dario.chuquilla@gmail.com>
2025-02-19 22:14:04 -0500

    Refactor SQL migrations to improve table structure and add new columns for paciente_planes_diagnostico and medico

--- sql_migrations.sql
+++ sql_migrations.sql
@@ -20,19 +20,22 @@ ALTER TABLE paciente_consulta
 ADD COLUMN cpo VARCHAR(255);
 
 CREATE TABLE
-  `paciente_planes_diagnostico` (
-    `pacplandiag_id` int (8) NOT NULL AUTO_INCREMENT,
-    `codi_pac` int (8) DEFAULT NULL,
-    `enfermedades` varchar(255) CHARACTER
-    SET
-      utf8 DEFAULT NULL,
-      `observaciones` varchar(255) CHARACTER
-    SET
-      utf8 DEFAULT NULL,
-      PRIMARY KEY (`pacplandiag_id`),
-      KEY `codi_pac` (`codi_pac`),
-      CONSTRAINT `paciente_plan_diagnostico_ibfk_1` FOREIGN KEY (`codi_pac`) REFERENCES `paciente` (`codi_pac`) ON DELETE CASCADE ON UPDATE CASCADE
-  ) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8 COLLATE = utf8_spanish_ci;
+	`paciente_planes_diagnostico` (
+		`pacplandiag_id` int (8) NOT NULL AUTO_INCREMENT,
+		`codi_pac` int (8) DEFAULT NULL,
+		`enfermedades` varchar(255) CHARACTER
+		SET
+			utf8 DEFAULT NULL,
+			`observaciones` varchar(255) CHARACTER
+		SET
+			utf8 DEFAULT NULL,
+			PRIMARY KEY (`pacplandiag_id`),
+			KEY `codi_pac` (`codi_pac`),
+			CONSTRAINT `paciente_plan_diagnostico_ibfk_1` FOREIGN KEY (`codi_pac`) REFERENCES `paciente` (`codi_pac`) ON DELETE CASCADE ON UPDATE CASCADE
+	) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8 COLLATE = utf8_spanish_ci;
 
 ALTER TABLE paciente_diagnostico
 ADD COLUMN predef VARCHAR(255);
+
+ALTER TABLE medico
+ADD COLUMN firma VARCHAR(255);
