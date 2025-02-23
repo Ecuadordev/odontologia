36cf73eb341fb0eb1b09255016a5270d6d2cc69c
Dario Chuquilla <dario.chuquilla@gmail.com>
2025-02-22 22:15:59 -0500

    Refactor Diagnostico_model for improved readability and consistency

--- application/models/Diagnostico_model.php
+++ application/models/Diagnostico_model.php
@@ -1,85 +1,82 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-/**
- *
- */
-/**
- *
- */
-class Diagnostico_model extends CI_Model{
+class Diagnostico_model extends CI_Model
+{
+	function getdiagnostico($data)
+	{
+		$this->db->from('enfermedad');
+		$this->db->where('esta_enf', S);
+		$queryTotal = $this->db->get();
 
+		$this->db->from('enfermedad');
+		$this->db->where('esta_enf', S);
+		if (isset($data['enfermedad'])) {
+			$this->db->having("desc_enf LIKE '%" . $data['enfermedad'] . "%'");
+		}
 
-  function getdiagnostico($data) {
-    $this->db->from('enfermedad');
-    $this->db->where('esta_enf',S);
-    $queryTotal = $this->db->get();
+		$queryLike = $this->db->get();
 
-    $this->db->from('enfermedad');
-    $this->db->where('esta_enf',S);
-    if (isset($data['enfermedad'])) {
-      $this->db->having("desc_enf LIKE '%".$data['enfermedad']."%'");
-    }
+		$this->db->from('enfermedad');
+		$this->db->where('esta_enf', S);
+		if ($data['length'] != -1) {
+			$this->db->limit($data['length'], $data['start']);
+		}
 
-    $queryLike = $this->db->get();
+		if (isset($data['orderCampo'])) {
+			$this->db->order_by($data['orderCampo'], $data['orderDireccion']);
+		}
 
-    $this->db->from('enfermedad');
-    $this->db->where('esta_enf',S);
-    if ($data['length']!=-1) {
-      $this->db->limit($data['length'],$data['start']);
-    }
+		if (isset($data['enfermedad'])) {
+			$this->db->having("desc_enf LIKE '%" . $data['enfermedad'] . "%'");
+		}
 
-    if (isset($data['orderCampo'])) {
-      $this->db->order_by($data['orderCampo'],$data['orderDireccion']);
-    }
+		$query = $this->db->get();
 
-    if (isset($data['enfermedad'])) {
-      $this->db->having("desc_enf LIKE '%".$data['enfermedad']."%'");
-    }
-
-    $query = $this->db->get();
-
-    $result = array();
-    $result['sEcho'] = $data['sEcho'];
-    $result['iTotalRecords'] = $queryTotal->num_rows();
-    $result['iTotalDisplayRecords'] = $queryLike->num_rows();
+		$result = array();
+		$result['sEcho'] = $data['sEcho'];
+		$result['iTotalRecords'] = $queryTotal->num_rows();
+		$result['iTotalDisplayRecords'] = $queryLike->num_rows();
 
 
-    $row = [];
-    foreach ($query->result() as $q) {
-      if ($q->esta_enf=='S') {
-        $estado = '<label class="label label-success">Activo</label>';
-      }elseif($q->esta_enf=='N'){
-        $estado = '<label class="label label-info">Inactivo</label>';
-      }
+		$row = [];
+		foreach ($query->result() as $q) {
+			if ($q->esta_enf == 'S') {
+				$estado = '<label class="label label-success">Activo</label>';
+			} elseif ($q->esta_enf == 'N') {
+				$estado = '<label class="label label-info">Inactivo</label>';
+			}
 
 
-      $botones = '<div class="btn-footer text-center">
-                  <a href="'.base_url('mantenimiento/diagnostico/editar/'.$q->codi_enf).'"class="btn btn-primary" style="padding:2px 5px;margin:0px 2px"> <i class="fa fa-edit"></i> </a>
-                    <button data-id="'.$q->codi_enf.'" class="anularcie10 btn btn-danger" style="padding:2px 5px;margin:0px 2px" >
+			$botones = '<div class="btn-footer text-center">
+                  <a href="' . base_url('mantenimiento/diagnostico/editar/' . $q->codi_enf) . '"class="btn btn-primary" style="padding:2px 5px;margin:0px 2px"> <i class="fa fa-edit"></i> </a>
+                    <button data-id="' . $q->codi_enf . '" class="anularcie10 btn btn-danger" style="padding:2px 5px;margin:0px 2px" >
                         <i class="glyphicon glyphicon-trash"></i> </button>';
 
-      $row[] = [$q->codi_enf,$q->desc_enf,$estado,$botones];
-    }
-    $result['aaData'] = $row;
-    return $result;
-  }
+			$row[] = [$q->codi_enf, $q->desc_enf, $estado, $botones];
+		}
+		$result['aaData'] = $row;
+		return $result;
+	}
 
-  public function agregardiagnostico($data) {
+	public function agregardiagnostico($data)
+	{
 
-    $this->db->insert('enfermedad',$data);
-    return $this->db->insert_id();
-  }
+		$this->db->insert('enfermedad', $data);
+		return $this->db->insert_id();
+	}
 
 
-  public function getdiagnosticoid($id) {
-      $this->db->where('codi_enf',$id);
-    $resultado = $this->db->get('enfermedad');
-    return $resultado->row();
-  }
+	public function getdiagnosticoid($id)
+	{
+		$this->db->where('codi_enf', $id);
+		$resultado = $this->db->get('enfermedad');
+		return $resultado->row();
+	}
 
-  public function update($id,$data){
-    $this->db->where('codi_enf',$id);
-    return $this->db->update('enfermedad',$data);
-  }
+	public function update($id, $data)
+	{
+		$this->db->where('codi_enf', $id);
+		return $this->db->update('enfermedad', $data);
+	}
 }
